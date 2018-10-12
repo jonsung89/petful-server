b2666244307ec0ad68f38a25aa1fb83ca4ff4ab1
jonsung89 <jonsung89@gmail.com>
2018-10-12 10:36:10 -0500

    Created basic dog and cat endpoints with dummy data

--- index.js
+++ index.js
@@ -10,6 +10,86 @@ const { dbConnect } = require('./db-mongoose');
 
 const app = express();
 
+
+const catData = [
+  {
+    imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Pickles',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Pinch',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Tabitha',
+    story: 'Thrown on the street'
+  }
+];
+
+const dogData = [
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Mickey',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Mason',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Spot',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  }
+];
+
+
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
@@ -21,6 +101,29 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+
+
+app.get('/api/cat', (req, res, next) => {
+  const catInfo = catData[0];
+  res.json(catInfo);
+});
+
+app.delete('/api/cat', (req, res, next) => {
+  catData.shift();
+  res.sendStatus(204);
+});
+
+app.get('/api/dog', (req, res, next) => {
+  const dogInfo = dogData[0];
+  res.json(dogInfo);
+});
+
+app.delete('/api/dog', (req, res, next) => {
+  dogData.shift();
+  res.sendStatus(204);
+});
+
+
 
 function runServer(port = PORT) {
   const server = app
